dcceefdf94dce2a24410b08dffcf8113eca1663f
Michael Cervone <MCervone14@gmail.com>
2024-03-26 12:44:21 -0500

    took out clerkjs

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,6 @@ import Head from "next/head";
 
 import Layout from "../components/Layout";
 import { Analytics } from "@vercel/analytics/react";
-import { ClerkProvider } from "@clerk/nextjs";
 
 /* Documentation for _app.js file
 
@@ -16,33 +15,28 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ClerkProvider>
-      <Layout>
-        <Head>
-          <meta
-            name="viewport"
-            content="initial-scale=1.0, width=device-width"
-          />
-          <meta name="robots" content="all" key="robotsAllowed" />
-          <meta
-            name="description"
-            content="We promote clean air, clean water, and healthy soils; a greener world; sustainable practices; renewable energy;
+    <Layout>
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="robots" content="all" key="robotsAllowed" />
+        <meta
+          name="description"
+          content="We promote clean air, clean water, and healthy soils; a greener world; sustainable practices; renewable energy;
           and conservation of worldwide ecosystems."
-            key="desc"
-          />
-          <link
-            rel="canonical"
-            href="https://ourplanetearth.eco"
-            key="canonical"
-          />
-          <link rel="icon" type="image/x-icon" href="/images/earth.png" />
-        </Head>
-        <main className="container">
-          <Component {...pageProps} />
-          <Analytics />
-        </main>
-      </Layout>
-    </ClerkProvider>
+          key="desc"
+        />
+        <link
+          rel="canonical"
+          href="https://ourplanetearth.eco"
+          key="canonical"
+        />
+        <link rel="icon" type="image/x-icon" href="/images/earth.png" />
+      </Head>
+      <main className="container">
+        <Component {...pageProps} />
+        <Analytics />
+      </main>
+    </Layout>
   );
 }
 
